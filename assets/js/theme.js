c475cae80d955ae24a04843fadfb4e7d7f14bbc3
haykal24 <moh.haikal02@gmail.com>
2024-10-10 16:04:03 +0700

    p

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -427,6 +427,30 @@ loader();
         });
     });
 
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        new Swiper('.service-slider', {
+          slidesPerView: 1,
+          spaceBetween: 30,
+          loop: true,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+          breakpoints: {
+            640: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          },
+        });
+      });
           /* Portfolio Isotope---case study*/
           $('.image_load').imagesLoaded(function () {
 
